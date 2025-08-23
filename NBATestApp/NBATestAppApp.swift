cbab32732ccd320a681699dd568d501b43ac2567
Sanjay Mali <malisanjay5433@gmail.com>
2025-08-23 21:07:42 +0530

    Initial Commit

---
+++ NBATestApp/NBATestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBATestAppApp.swift
+//  NBATestApp
+//
+//  Created by Sanjay Mali on 23/08/25.
+//
+
+import SwiftUI
+
+@main
+struct NBATestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
